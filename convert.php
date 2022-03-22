74e6c0f89a3eb49e63b0a10eafe033bfedf9d4b0
NSExpert <90751790+amureke@users.noreply.github.com>
2022-03-23 04:01:48 +0900

    update parse

--- convert.php
+++ convert.php
@@ -1,17 +1,19 @@
 <?php
 
 const SEARCH_KEY = "19.00DOOR";
-
 const SEARCH_KEY_COLUMN = 3;
 const QTY_COLUMN = 1;
 const LPX_COLUMN = 8;
 const LPY_COLUMN = 7;
+const LPZ = 19;
 const DIRECTION_COLUMN = 13;
+const OUTPUT_DIR = "output";
+const INPUT_DIR = "input";
 
 function getXYDirection($pts_file) {
     $ret = [];
     $row = 1;
-    echo "-------- File Reading ($pts_file)--------- " . "<br/>";
+    echo "---- File Reading ($pts_file) ----" . "<br/>";
     if (($handle = fopen($pts_file, "r")) !== FALSE) {
         while (($one_row = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $num = count($one_row);
@@ -33,39 +35,80 @@ function getXYDirection($pts_file) {
             }
         }
         fclose($handle);
-        echo "-------- Get Info from ($pts_file)---------" . "<br/>";
+        echo "---- Get Info from ($pts_file) ----" . "<br/>";
     }
     return $ret;
 }
 
-function replaceXY($bpp_file, $x, $y, $qty, $direction) {
-    $search_words = ["LPX", "LPY"];
-    $replace_words = [$x, $y];
-
+function replaceXY($bpp_file, $data) {
+    $search_words = ["LPX", "LPY", "LPZ"];
+    
     $path_parts = pathinfo($bpp_file);
     $filename = $path_parts['filename'];
+    echo "-------- File Reading ($bpp_file) --------- " . "<br/>";
+    if (($handle = fopen($bpp_file, "r")) === FALSE) {
+        echo "Cannot open file ($bpp_file)\n";
+        return false;
+    }
 
-    $new_file = sprintf("%s_%d_%d_%d_%s.bpp", $filename, $x, $y, $qty, $direction);
-    $write_handle = fopen($new_file, "w");
-    echo "-------- File Reading ($bpp_file)--------- " . "<br/>";
-    if (($handle = fopen($bpp_file, "r")) !== FALSE) {
+    foreach($data as $one_data) {
+        fseek($handle,0);
+        $x = $one_data['lpx'];
+        $y = $one_data['lpy'];
+        $qty = $one_data['qty'];
+        $slug = $one_data['slug'];
+
+        $pattern_x = "/PAN=LPX\|(\d)+\|\|/";
+        $replace_x = "PAN=LPX|$x||";
+        
+        $pattern_y = "/PAN=LPY\|(\d)+\|\|/";
+        $replace_y = "PAN=LPY|$y||";
+        
+        $qty = $one_data["qty"];
+        $slug = $one_data["slug"];
+        $replace_words = [$x, $y, LPZ];
+
+        $new_file = OUTPUT_DIR . DIRECTORY_SEPARATOR . sprintf("%s_%d_%d_%d_%s.bpp", $filename, $x, $y, $qty, $slug);
+        if(!file_exists(dirname($new_file)))
+            mkdir(dirname($new_file), 0777, true);
+        $write_handle = fopen($new_file, "w");
         while (($one_row = fgets($handle)) !== FALSE) {
-            $replace_row = str_replace($search_words, $replace_words, $one_row);
-            // Write $replace_row to our opened file.
-            if (fwrite($write_handle, $replace_row) === FALSE) {
+            $replace_x_row = preg_replace($pattern_x, $replace_x, $one_row);
+            $replace_y_row = preg_replace($pattern_y, $replace_y, $replace_x_row);
+            // Write replace string to our opened file.
+            if (fwrite($write_handle, $replace_y_row) === FALSE) {
                 echo "Cannot write to file ($filename)\n";
             }
         }
-        fclose($handle);
+        fclose($write_handle);    
+        echo "---------------- File Writing ($new_file) -----------------" . "<br/>";
     }
-    fclose($write_handle);    
-    echo "-------- File Creating ($new_file)---------" . "<br/>";
+    
+    fclose($handle);
 }
 
 function slugify($string){
     return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $string), '-'));
 }
 
-$data = getXYDirection("Jean_repaired.PTS");
+function getBPPfiles($in_dir) {
+    $bpp_files = glob($in_dir . DIRECTORY_SEPARATOR . "*.bpp");
+    return $bpp_files;
+}
 
-replaceXY("Monaco.bpp", $data[0]["lpx"], $data[0]["lpy"], $data[0]["qty"], $data[0]["slug"]);
+function getPTSfile($in_dir) {
+    $pts_files = glob($in_dir . DIRECTORY_SEPARATOR . "*.PTS");
+    return $pts_files;
+}
+
+function start() {
+    $pts_files = getPTSfile(INPUT_DIR);
+    $bpp_files = getBPPfiles(INPUT_DIR);
+    foreach($pts_files as $pts_file) {
+        $data = getXYDirection($pts_file);
+        foreach($bpp_files as $bpp_file)
+            replaceXY($bpp_file, $data);
+    }
+}
+
+start();
